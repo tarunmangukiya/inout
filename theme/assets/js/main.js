cb7b38a2cde2431494cbeb4ddaea0a36edbd61e7
Tarun Mangukiya <tarunmangukiya@hotmail.com>
2016-06-12 15:32:31 +0530

    Inout Home Page

--- theme/assets/js/main.js
+++ theme/assets/js/main.js
@@ -1,4 +1,17 @@
 $(function () {
+
+	$(window).on("scroll", function(e) {
+	    var wrap = $(".static-header");
+
+	    //console.log($(this).scrollTop());
+
+	    if ($(this).scrollTop() > 500) {
+	        wrap.addClass("fix-header");
+	    } else {
+	        wrap.removeClass("fix-header");
+	    }
+	});
+
 	if($('.file-input').length){
 		$('.file-input input[type=file]').on('click', function(e) {
 			e.stopPropagation();
